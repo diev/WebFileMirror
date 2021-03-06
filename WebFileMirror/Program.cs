8311164aa2d3b0f2311947d22db5558867936440
Dmitrii Evdokimov <diev@mail.ru>
2021-03-06 22:12:57 +0300

    Add downloading

--- WebFileMirror/Program.cs
+++ WebFileMirror/Program.cs
@@ -19,6 +19,7 @@ using System;
 using System.Configuration;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -39,6 +40,8 @@ namespace WebFileMirror
             }
 
             Crawler1(uri, mirror);
+
+            Console.WriteLine("Press Enter to exit.");
             Console.ReadLine();
         }
 
@@ -50,6 +53,7 @@ namespace WebFileMirror
                 <a class="col-md-19 offset-md-1 rubric_title" href="doc?number=40">Вопросы по Инструкции № 136-И</a>
             </div> */
 
+            _client.BaseAddress = uri;
             string page = GetPage(uri);
 
             string start = "<h2 class=\"h3\">КО</h2>";
@@ -76,7 +80,7 @@ namespace WebFileMirror
                     Directory.CreateDirectory(dir);
                 }
 
-                Crawler2(uri + url, dir);
+                Crawler2(url, dir);
             }
         }
 
@@ -145,24 +149,14 @@ namespace WebFileMirror
               </div>
             </div> */
 
-            var titleR = new Regex(@"<div class=""question_title"">(?<title>.+)<\/div>");
-
-            //var hrefQ = new Regex(@"<a .*href=""\/Queries\/UniDbQuery\/File\/(?<folder>\d*)\/(?<file>\d*)\/Q"">\s*Вопрос\s*<\/a>");
-            //var hrefA = new Regex(@"<a .*href=""\/Queries\/UniDbQuery\/File\/(?<folder>\d*)\/(?<file>\d*)\/A"">\s*Ответ\s*<\/a>");
-            var hrefR = new Regex(@"<a .*href=""\/Queries\/UniDbQuery\/File\/(?<folder>\d*)\/(?<file>\d*)\/Q"">\s*Вопрос\s*<\/a>");
-
-            var dateR = new Regex(@"Дата последнего обновления: (?<date>\d{2}\.\d{2}\.\d{4})");
-
-            Match m;
-
             string title;
-            string folder;
-            //string fileQ;
-            //string fileA;
-            string file;
             DateTime updated;
 
-            m = titleR.Match(section);
+            string url;
+            string folder;
+            string file;
+
+            Match m = new Regex(@"<div class=""question_title"">(?<title>.+)<\/div>").Match(section);
             if (m.Success)
             {
                 title = m.Result("${title}");
@@ -172,9 +166,10 @@ namespace WebFileMirror
                 throw new Exception($"HTML изменился - невозможно определить название документа.");
             }
 
-            m = hrefR.Match(section);
+            m = new Regex(@"<a .*href=""(?<url>\/Queries\/UniDbQuery\/File\/(?<folder>\d*)\/(?<file>\d*)\/)Q"">\s*Вопрос\s*<\/a>").Match(section);
             if (m.Success)
             {
+                url = m.Result("${url}");
                 folder = m.Result("${folder}");
                 file = m.Result("${file}");
             }
@@ -183,7 +178,7 @@ namespace WebFileMirror
                 throw new Exception($"HTML изменился - невозможно определить файл вопроса.");
             }
 
-            m = dateR.Match(section);
+            m = new Regex(@"Дата последнего обновления: (?<date>\d{2}\.\d{2}\.\d{4})").Match(section);
             if (m.Success)
             {
                 updated = DateTime.Parse(m.Result("${date}"));
@@ -193,7 +188,83 @@ namespace WebFileMirror
                 throw new Exception($"HTML изменился - невозможно определить дату последнего обновления.");
             }
 
-            File.WriteAllText(Path.Combine(path, $"{folder}_{file}.txt"), $"Updated: {updated}");
+            var sb = new StringBuilder();
+            sb.AppendLine(title);
+            sb.AppendLine($"Updated: {updated:d}");
+            sb.AppendLine();
+            sb.AppendLine($"Q: {url}Q");
+            sb.AppendLine($"A: {url}A");
+
+            /*
+            Ответ ДФМиВК от 16.04.2018 № 12-3-5/2688 на запрос КО
+            Updated: 18.12.2018
+            */
+
+            string item = $"{folder}_{file}_{updated:yyyy-MM-dd}";
+            path = Path.Combine(path, item);
+            Directory.CreateDirectory(path);
+
+            File.WriteAllText(Path.Combine(path, item + ".info"), sb.ToString());
+
+            WriteFile(url + "Q", path, true, true);
+            WriteFile(url + "A", path, true, true);
+        }
+
+        private static void WriteFile(string uri, string path, bool writeHeaders, bool writeContent)
+        {
+            using (var responseStream = _client.OpenRead(uri))
+            {
+                var responseHeaders = _client.ResponseHeaders;
+
+                string file;
+                var rexpFilename = new Regex("filename=(?<filename>.*);");
+                Match m = rexpFilename.Match(responseHeaders.Get("Content-Disposition"));
+                if (m.Success)
+                {
+                    file = Path.Combine(path, m.Result("${filename}"));
+                }
+                else
+                {
+                    throw new Exception($"Сервер изменился - невозможно определить имя файла.");
+                }
+
+                if (writeHeaders)
+                {
+                    var sb = new StringBuilder();
+                    for (int i = 0; i < responseHeaders.Count; i++)
+                    {
+                        sb.AppendLine($"{responseHeaders.GetKey(i)}: {responseHeaders.Get(i)}");
+                    }
+
+                    /* (Q)
+                    Connection: keep-alive
+                    Keep-Alive: timeout=60
+                    Content-Length: 258694
+                    Cache-Control: private
+                    Content-Type: application/pdf
+                    Date: Sat, 06 Mar 2021 15:33:34 GMT
+                    Last-Modified: Tue, 18 Dec 2018 14:42:19 GMT
+                    Set-Cookie: __ddg1=ZI71Ug0pN298UWQ6HY41; Domain=.cbr.ru; HttpOnly; Path=/; Expires=Sun, 06-Mar-2022 15:33:33 GMT,ASPNET_SessionID=oy3kclmvasmb1ybo5tdkz0r5; path=/; HttpOnly; SameSite=Lax
+                    Server: ddos-guard
+                    X-AspNetMvc-Version: 5.2
+                    X-Zoom-Title: 0J7RgtCy0LXRgiDQlNCk0JzQuNCS0Jog0L7RgiAxNi4wNC4yMDE4IOKEliAxMi0zLTUvMjY4OCDQvdCwINC30LDQv9GA0L7RgSDQmtCeICjRgtC10LrRgdGCINCy0L7Qv9GA0L7RgdCwKQ==
+                    Content-Disposition: inline; filename=q136i_20180416_2688.pdf; filename*=utf-8''q136i_20180416_2688.pdf
+                    X-AspNet-Version: 4.0.30319
+                    X-Powered-By: ASP.NET
+                    */
+
+                    File.WriteAllText(file + ".info", sb.ToString());
+                }
+
+                if (writeContent)
+                { 
+                    using (var fileStream = new FileStream(file, FileMode.OpenOrCreate))
+                    {
+                        responseStream.CopyTo(fileStream);
+                        fileStream.Flush();
+                    }
+                }
+            }
         }
 
         private static string GetPage(string uri)
