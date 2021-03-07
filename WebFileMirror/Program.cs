50bfee7c4c129a15ea95b096bd3721008fb95514
Dmitrii Evdokimov <diev@mail.ru>
2021-03-07 06:31:18 +0300

    Clean code

--- WebFileMirror/Program.cs
+++ WebFileMirror/Program.cs
@@ -19,7 +19,6 @@ using System;
 using System.Configuration;
 using System.IO;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -54,6 +53,7 @@ namespace WebFileMirror
             </div> */
 
             _client.BaseAddress = uri;
+            _client.Encoding = Encoding.UTF8;
             string page = GetPage(uri);
 
             string start = "<h2 class=\"h3\">КО</h2>";
@@ -67,9 +67,6 @@ namespace WebFileMirror
 
             foreach (Match m in hrefs.Matches(section))
             {
-                //var url = m.Groups[1].ToString();
-                //var title = m.Groups[2].ToString();
-
                 string url = m.Result("${url}");
                 string title = m.Result("${title}");
 
@@ -152,9 +149,8 @@ namespace WebFileMirror
             string title;
             DateTime updated;
 
-            string url;
-            string folder;
-            string file;
+            string urlQ, urlA;
+            string folder, file;
 
             Match m = new Regex(@"<div class=""question_title"">(?<title>.+)<\/div>").Match(section);
             if (m.Success)
@@ -169,7 +165,9 @@ namespace WebFileMirror
             m = new Regex(@"<a .*href=""(?<url>\/Queries\/UniDbQuery\/File\/(?<folder>\d*)\/(?<file>\d*)\/)Q"">\s*Вопрос\s*<\/a>").Match(section);
             if (m.Success)
             {
-                url = m.Result("${url}");
+                string url = "http://www.cbr.ru" + m.Result("${url}");
+                urlQ = url + "Q";
+                urlA = url + "A";
                 folder = m.Result("${folder}");
                 file = m.Result("${file}");
             }
@@ -188,121 +186,130 @@ namespace WebFileMirror
                 throw new Exception($"HTML изменился - невозможно определить дату последнего обновления.");
             }
 
-            var sb = new StringBuilder();
-            sb.AppendLine(title);
-            sb.AppendLine($"Updated: {updated:d}");
-            sb.AppendLine();
-            sb.AppendLine($"Q: {url}Q");
-            sb.AppendLine($"A: {url}A");
+            Console.WriteLine($"  {updated:yyyy-MM-dd}: {title}");
+
+            string item = $"{folder}_{file}_{updated:yyyy-MM-dd}";
+            //path = Path.Combine(path, item);
+            path = Path.Combine(path, $"{updated:yyyy-MM-dd} {GetValidFileName(title)}");
+            Directory.CreateDirectory(path);
+
+            var sb = new StringBuilder()
+                .AppendLine(title)
+                .AppendLine($"Updated: {updated:d}")
+                .AppendLine()
+                .AppendLine($"Q: {urlQ}")
+                .AppendLine($"A: {urlA}");
 
             /*
             Ответ ДФМиВК от 16.04.2018 № 12-3-5/2688 на запрос КО
             Updated: 18.12.2018
-            */
 
-            string item = $"{folder}_{file}_{updated:yyyy-MM-dd}";
-            path = Path.Combine(path, item);
-            Directory.CreateDirectory(path);
+            Q: http://www.cbr.ru/Queries/UniDbQuery/File/104594/72/Q
+            A: http://www.cbr.ru/Queries/UniDbQuery/File/104594/72/A
+            */
 
             File.WriteAllText(Path.Combine(path, item + ".info"), sb.ToString());
 
-            WriteFile(url + "Q", path, true, true);
-            WriteFile(url + "A", path, true, true);
+            StoreFile(urlQ, path);
+            StoreFile(urlA, path);
         }
 
-        private static void WriteFile(string uri, string path, bool writeHeaders, bool writeContent)
+        private static void StoreFile(string uri, string path)
         {
-            using (var responseStream = _client.OpenRead(uri))
+            var info = new StringBuilder();
+            var headers = GetHeaders(uri);
+            string file = Path.Combine(path, GetFileName(headers["Content-Disposition"]));
+            long size = long.Parse(headers["Content-Length"]);
+
+            var fi = new FileInfo(file);
+            if (!fi.Exists)
             {
-                var responseHeaders = _client.ResponseHeaders;
-
-                string file;
-                var rexpFilename = new Regex("filename=(?<filename>.*);");
-                Match m = rexpFilename.Match(responseHeaders.Get("Content-Disposition"));
-                if (m.Success)
+                _client.DownloadFile(uri, file);
+                info.AppendLine(uri).Append(headers);
+                File.WriteAllText(file + ".info", info.ToString());
+            }
+            else if (fi.Length != size) //TODO new update
+            {
+                int n = 1;
+                do
                 {
-                    file = Path.Combine(path, m.Result("${filename}"));
+                    file = $"{Path.Combine(fi.DirectoryName, Path.GetFileNameWithoutExtension(fi.Name))} ({++n}){fi.Extension}";
                 }
-                else
-                {
-                    throw new Exception($"Сервер изменился - невозможно определить имя файла.");
-                }
+                while (File.Exists(file));
 
-                if (writeHeaders)
-                {
-                    var sb = new StringBuilder();
-                    for (int i = 0; i < responseHeaders.Count; i++)
-                    {
-                        sb.AppendLine($"{responseHeaders.GetKey(i)}: {responseHeaders.Get(i)}");
-                    }
+                _client.DownloadFile(uri, file);
+                info.AppendLine($"-{n}-").Append(headers);
+                File.AppendAllText(file + ".info", info.ToString());
+            }
+        }
 
-                    /* (Q)
-                    Connection: keep-alive
-                    Keep-Alive: timeout=60
-                    Content-Length: 258694
-                    Cache-Control: private
-                    Content-Type: application/pdf
-                    Date: Sat, 06 Mar 2021 15:33:34 GMT
-                    Last-Modified: Tue, 18 Dec 2018 14:42:19 GMT
-                    Set-Cookie: __ddg1=ZI71Ug0pN298UWQ6HY41; Domain=.cbr.ru; HttpOnly; Path=/; Expires=Sun, 06-Mar-2022 15:33:33 GMT,ASPNET_SessionID=oy3kclmvasmb1ybo5tdkz0r5; path=/; HttpOnly; SameSite=Lax
-                    Server: ddos-guard
-                    X-AspNetMvc-Version: 5.2
-                    X-Zoom-Title: 0J7RgtCy0LXRgiDQlNCk0JzQuNCS0Jog0L7RgiAxNi4wNC4yMDE4IOKEliAxMi0zLTUvMjY4OCDQvdCwINC30LDQv9GA0L7RgSDQmtCeICjRgtC10LrRgdGCINCy0L7Qv9GA0L7RgdCwKQ==
-                    Content-Disposition: inline; filename=q136i_20180416_2688.pdf; filename*=utf-8''q136i_20180416_2688.pdf
-                    X-AspNet-Version: 4.0.30319
-                    X-Powered-By: ASP.NET
-                    */
+        private static WebHeaderCollection GetHeaders(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "HEAD";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                return response.Headers;
 
-                    File.WriteAllText(file + ".info", sb.ToString());
-                }
+                /* (Q)
+                Connection: keep-alive
+                Keep-Alive: timeout=60
+                Content-Length: 258694
+                Cache-Control: private
+                Content-Type: application/pdf
+                Date: Sat, 06 Mar 2021 15:33:34 GMT
+                Last-Modified: Tue, 18 Dec 2018 14:42:19 GMT
+                Set-Cookie: __ddg1=ZI71Ug0pN298UWQ6HY41; Domain=.cbr.ru; HttpOnly; Path=/; Expires=Sun, 06-Mar-2022 15:33:33 GMT,ASPNET_SessionID=oy3kclmvasmb1ybo5tdkz0r5; path=/; HttpOnly; SameSite=Lax
+                Server: ddos-guard
+                X-AspNetMvc-Version: 5.2
+                X-Zoom-Title: 0J7RgtCy0LXRgiDQlNCk0JzQuNCS0Jog0L7RgiAxNi4wNC4yMDE4IOKEliAxMi0zLTUvMjY4OCDQvdCwINC30LDQv9GA0L7RgSDQmtCeICjRgtC10LrRgdGCINCy0L7Qv9GA0L7RgdCwKQ==
+                Content-Disposition: inline; filename=q136i_20180416_2688.pdf; filename*=utf-8''q136i_20180416_2688.pdf
+                X-AspNet-Version: 4.0.30319
+                X-Powered-By: ASP.NET
+                */
+            }
+        }
 
-                if (writeContent)
-                { 
-                    using (var fileStream = new FileStream(file, FileMode.OpenOrCreate))
-                    {
-                        responseStream.CopyTo(fileStream);
-                        fileStream.Flush();
-                    }
-                }
+        private static string GetFileName(string contentDisposition)
+        {
+            /* inline; filename=q136i_20180416_2688.pdf; filename*=utf-8''q136i_20180416_2688.pdf */
+            Match m = new Regex("filename=(?<filename>.*);").Match(contentDisposition);
+            if (m.Success)
+            {
+                return m.Result("${filename}");
+            }
+            else
+            {
+                throw new Exception($"Сервер изменился - невозможно определить имя файла.");
             }
         }
 
         private static string GetPage(string uri)
         {
-            string page;
-
 #if DEBUG
-            string file = GetFileName(uri);
-
+            string file = GetValidFileName(uri) + ".html";
             if (File.Exists(file))
             {
-                page = File.ReadAllText(file, Encoding.UTF8);
+                return File.ReadAllText(file, Encoding.UTF8);
             }
             else
             {
-                byte[] content = _client.DownloadData(uri);
-                page = Encoding.UTF8.GetString(content);
+                string page = _client.DownloadString(uri);
                 File.WriteAllText(file, page, Encoding.UTF8);
+                return page;
             }
 #else
-            byte[] content = _client.DownloadData(uri);
-            page = Encoding.UTF8.GetString(content);
+            return _client.DownloadString(uri);
 #endif
-
-            return page;
         }
 
-        private static string GetFileName(string uri)
+        private static string GetValidFileName(string uri)
         {
-            return uri
-                .Replace('\\', '_')
-                .Replace('/', '_')
-                .Replace(':', '_')
-                .Replace('*', '_')
-                .Replace('?', '_')
-                .Replace('\"', '_')
-                .Replace('\'', '_')
-                + ".html";
+            string file = uri;
+            Array.ForEach(Path.GetInvalidFileNameChars(), 
+                c => file = file.Replace(c, '_'));
+
+            return file;
         }
     }
 }
